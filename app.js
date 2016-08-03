f8f60756a05daae195590923f760af314070dfc2
Sean Hornsby <sean.hornsby@gmail.com>
2016-08-03 13:44:50 -0700

    Attaches app.js to mongodb. #1

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+
 var app = express();
 var incomes = require('./routes/incomes.js');
 
-app.use('/incomes', incomes);
-app.use(express.static('app'));
+var user = process.env.MDB;
+var pw = process.env.MDBPW;
+var url = 'mongodb://' + user + ':' + pw + '@ds029745.mlab.com:29745/heroku_8xrhcd53';
 
-app.listen(8000, () => console.log("8000"));
+MongoClient.connect(url, function(err, db) {
+
+  if(err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  express()
+    .use('/incomes', incomes)
+    .use(express.static('app'))
+    .listen(8000);
+})
