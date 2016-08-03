93b87722c6f0aa24e683016cd6fb0a961a661c95
Sean Hornsby <sean.hornsby@gmail.com>
2016-08-03 15:39:10 -0700

    Adds get and post for incomes. #1

--- routes/income-data.js
+++ routes/income-data.js
@@ -3,7 +3,17 @@
 module.exports = function incomeData(db) {
   var collection = db.collection('incomes');
 
-  return {findAll};
+  return {create, findAll};
+
+  function create(income, callback) {
+    collection.insert(income, function(err, result) {
+      if (err) {
+        return callback(err);
+      }
+      var created = result.ops[0];
+      callback(null, created);
+    });
+  }
 
   function findAll(callback) {
     collection.find({}).toArray(callback);
--- routes/incomes.js
+++ routes/incomes.js
@@ -12,6 +12,17 @@ module.exports = function incomes(income) {
       res.json(docs);
     })
   })
-
+  router.post('/', function(req, res, next) {
+    var data = req.body;
+    if (!data.name || !data.amount) {
+      return res.sendStatus(400);
+    }
+    income.create(data, function(err, doc) {
+      if (err) {
+        return next(err);
+      }
+      res.status(201).json(doc);
+    })
+  })
   return router;
 };
